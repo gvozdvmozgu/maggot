8f88cd73bb203edb26726671048c3a906c2135b9
Andrey Nikolaev <gvozdvmozgu@gmail.com>
2025-04-04 10:38:39 -0700

    Update lib.rs

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -4,7 +4,7 @@ use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
 pub fn hydrate() {
-    _ = console_log::init_with_level(log::Level::Debug);
+    let _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
     leptos::mount_to_body(App);
 }
